99f011a9c07d04655f80e8d23fa45388e6b42c27
stroligo <gabrielstroligo@gmail.com>
2022-12-11 03:47:32 +0000

    Update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -88,7 +88,7 @@ function HomePage() {
           <span className="sm:ml-3">
             <button
               type="button"
-              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="inline-flex items-center rounded-md border border-transparent bg-blue2 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-orange focus:outline-none"
             >
               <CheckIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
               Publish
