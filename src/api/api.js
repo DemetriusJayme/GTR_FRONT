5575e8a4cae7e9775884405f9bdd2853c5ea5b50
Arash Kaffashi <arash_kaffashi@hotmail.com>
2022-12-12 13:45:30 -0400

    fix: api production url

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:8080",
-  production: "https://gtr-server.cyclic.app",
+  production: "https://gtr-back.cyclic.app",
 };
 
 //instancia que sabe qual é a BASEURL QUE DEVE SER USADA NAS REQUISIÇÕES DO AXIOS
