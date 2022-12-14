7a8dc527e48405fcab084490b9ac4c72abc6a5a9
Arash Kaffashi <arash_kaffashi@hotmail.com>
2022-12-14 16:26:41 -0400

    fix: dropdown

--- src/components/task/MembersDropDownMenu.js
+++ src/components/task/MembersDropDownMenu.js
@@ -1,26 +1,24 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../../contexts/authContext";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
-export default function MembersDropDownMenu({ onChange }) {
+export default function MembersDropDownMenu({ onChange, selected = [] }) {
   const { loggedInUser } = useContext(AuthContext);
   let team = loggedInUser?.user.team || [];
-  let [selected, setSelected] = useState({});
 
-  function getSelected(selected) {
-    return team.filter((_, index) => !!selected[index]);
-  }
+  function handleChange(id) {
+    let index = selected.indexOf(id);
+    let result;
 
-  function handleChange(index) {
-    let resSelected = {
-      ...selected,
-      ...{ [index]: index in selected ? !selected[index] : true },
-    };
-    setSelected(resSelected);
+    if (index === -1) {
+      result = [...selected, id];
+    } else {
+      result = selected.filter((_id) => _id !== id);
+    }
     onChange({
       target: {
         name: "members",
-        value: [...getSelected(resSelected)].map((user) => user._id),
+        value: result,
       },
     });
   }
@@ -35,48 +33,51 @@ export default function MembersDropDownMenu({ onChange }) {
     <div className="mt-4 flex flex-wrap sm:flex-nowrap items-start">
       <div className="group btn items-end relative">
         <span htmlFor="members">Members</span>
-        <ChevronDownIcon className="h-4 w-4 ml-1 transform group-hover:-rotate-180 transition duration-150 ease-in-out" />
-
-        <table
-          className="left-0 top-full mt-1 rounded-md drop-shadow table-auto transform scale-0 group-hover:scale-100 absolute
-          transition duration-300 ease-in-out origin-top">
-          <thead>
-            <tr>
-              <th className="px-2 py-1"></th>
-              <th className="px-2 py-1">Name</th>
-              <th className="px-2 py-1">Registration</th>
-            </tr>
-          </thead>
-          <tbody>
-            {team.map((user, index) => (
-              <tr
-                key={user._id}
-                className="hover:bg-slate-100 cursor-pointer"
-                onClick={() => handleChange(index)}
-                onMouseDown={disallowDoubleClickSelection}>
-                <td className="px-2 py-1">
-                  <input
-                    type="checkbox"
-                    className="mr-2"
-                    name="members"
-                    id="members"
-                    checked={selected[index] || false}
-                    onChange={() => {}}
-                  />
-                </td>
-                <td className="px-2 py-1 whitespace-nowrap">{user.name}</td>
-                <td className="px-2 py-1">{user.registration}</td>
+        <ChevronDownIcon className="h-4 w-4 ml-1 transform group-hover:-rotate-180 transition duration-300 ease-in-out" />
+        <div className="absolute left-0 top-full overflow-hidden group-hover:overflow-auto h-36 pt-1 rounded-md drop-shadow ">
+          <table
+            className="table-auto transform opacity-0 group-hover:opacity-100
+            transition duration-300 ease-in-out origin-top">
+            <thead>
+              <tr>
+                <th className="px-2 py-1"></th>
+                <th className="px-2 py-1">Name</th>
+                <th className="px-2 py-1">Registration</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {team.map((user) => (
+                <tr
+                  key={crypto.randomUUID()}
+                  className="hover:bg-slate-100 cursor-pointer"
+                  onClick={() => handleChange(user._id)}
+                  onMouseDown={disallowDoubleClickSelection}>
+                  <td className="px-2 py-1">
+                    <input
+                      type="checkbox"
+                      className="mr-2"
+                      name="members"
+                      id="members"
+                      checked={selected.includes(user._id)}
+                      onChange={() => {}}
+                    />
+                  </td>
+                  <td className="px-2 py-1 whitespace-nowrap">{user.name}</td>
+                  <td className="px-2 py-1">{user.registration}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
       <ul className="list-disc flex flex-wrap w-full sm:gap-6 ml-6">
-        {getSelected(selected).map(({ _id, name }) => (
-          <li key={_id} className="ml-6 w-full sm:w-max ">
-            {name}
-          </li>
-        ))}
+        {team
+          .filter((user) => selected.includes(user._id))
+          .map(({ _id, name }) => (
+            <li key={crypto.randomUUID()} className="ml-6 w-full sm:w-max ">
+              {name}
+            </li>
+          ))}
       </ul>
     </div>
   );
