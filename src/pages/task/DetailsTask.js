e9a25fcd6d12dfac918d3c77334ca0c20c42f357
stroligo <gabrielstroligo@gmail.com>
2022-12-15 13:06:58 -0300

    first commit

--- src/pages/task/DetailsTask.js
+++ src/pages/task/DetailsTask.js
@@ -25,21 +25,28 @@ function DetailsTask() {
   return (
     <>
       <section>
-        <div className="md:grid md:grid-cols-4 gap-8 p-0  mb-8">
+        <div className="md:grid md:grid-cols-4 gap-4 p-0  mb-4">
           <div className=" col-span-3">
-            <h1>
-              {task.name} / {task.status}
+            <h1 className="mb-0 flex">
+              {task.name}
+              <span className="text-orange text-sm ml-2">{task.status}</span>
             </h1>
 
-            <h4 className="author mb-4">Author: {task.author.name}</h4>
+            <h4 className="author text-sm">
+              <span className="font-bold">Author:</span> {task.author.name}
+            </h4>
           </div>
 
           <div>
-            <p className="">
-              Priority: <span className="uppercase"> {task.priority}</span>
-            </p>
+            <div className="">
+              <span className="font-bold">Priority: </span>
+              <span className="tag"> {task.priority}</span>
+            </div>
             {/* codicional cor */}
-            <p className="">Deadline: {task.deadline}</p>
+            <div className="">
+              <span className="font-bold">Deadline:</span>{" "}
+              <span className="font-bold text-orange ">{task.deadline}</span>
+            </div>
           </div>
         </div>
 
@@ -48,35 +55,43 @@ function DetailsTask() {
             <p>{task.description}</p>
           </div>
           <div className=" col-span-1">
-            <p>{task.estimated}h</p>
+            <span className="font-bold">Estimated:</span>{" "}
+            <span>{task.estimated}h</span>
           </div>
         </div>
-
-        <div className="md:grid md:grid-cols-2 gap-4  bg-white p-0 rounded-md">
+        <hr className="mb-0 mt-2" />
+        <div className="md:grid md:grid-cols-2 mb-4 gap-4 pt-0 bg-white p-0 rounded-md">
           <div>
             {/* CONDICIONAL APRESENTAR */}
             <ul className="members">
-              <h3>Members:</h3>
+              <p className="mb-1 font-bold text-md text-blue">Members:</p>
               {task.members.map((member) => (
-                <li key={member._id}>{member.name}</li>
+                <li className="ml-2" key={member._id}>
+                  {member.name}
+                </li>
               ))}
             </ul>
           </div>
           <div>
             <ul className="tags">
-              <h3>Tags:</h3>
+              <p className="mb-1 font-bold text-md text-blue">Tags:</p>
               {task.tags.map((tag) => (
-                <li key={crypto.randomUUID()}>{tag}</li>
+                <li className="tag" key={crypto.randomUUID()}>
+                  {tag}
+                </li>
               ))}
             </ul>
           </div>
         </div>
       </section>
-      <section className="flex justify-center gap-4 items-center">
-        <button className="btn m-0">ACCEPT</button>
-        <button className="btn m-0">REJECT</button>
-        <button className="btn m-0">EDIT</button>
-        <button className="btn m-0">ARCHIVE</button>
+
+      <section className="flex justify-end items-right bg-gray-100 p-2  mt-4">
+        <div className="flex justify-center gap-2 items-center">
+          <button className="btn mt-0">ACCEPT</button>
+          <button className="btn mt-0">REJECT</button>
+          <button className="btn mt-0">EDIT</button>
+          <button className="btn mt-0">ARCHIVE</button>
+        </div>
       </section>
     </>
   );
